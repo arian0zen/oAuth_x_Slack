d1a378b04bb8515f89b1089c808ca76aba9197d2
Arian Shaikh <ariansk90@gmail.com>
2022-09-25 04:12:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(express.static("public"));
 
 
-  mongoose.connect("mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+"@cluster0.q9xcxma.mongodb.net/"+process.env.DB_NAME);
+mongoose.connect("mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+"@cluster0.q9xcxma.mongodb.net/"+process.env.DB_NAME);
 
 
 
 const usersSchema = mongoose.Schema({
-  name: Number,
+  name: String,
   token: String,
 });
 
@@ -64,8 +64,13 @@ app.listen(port, () => {
 });
 
 // app.get("/api/token", async (req, res) => {
-//   const vari = await axios .post(
-//       `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=10UT8TO9Z9KDA2L4KI9W3R8PQEQ2O280`
-//     ).catch(Error)
-//     // console.log(vari.data.access_token)
+//   const newUSer = new User({
+//     name: "arian",
+//     token: "arian",
+//   });
+//   newUSer.save().then((item) => {
+//     res.json({
+//       message: "success"
+//     });
+//   });
 // });
