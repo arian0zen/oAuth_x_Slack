a082f0fb34e8205e96b8539712328b9ec26a6d74
Arian Shaikh <ariansk90@gmail.com>
2022-09-24 23:54:17 +0530

    added redirect urls

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const { response } = require("express");
 const app = express();
-
 app.use(express.static("public"));
+
+const uri =
+  "mongodb+srv://" +
+  process.env.DB_USERNAME +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@cluster0.q9xcxma.mongodb.net/" +
+  process.env.DB_NAME;
+
+const connect = async function () {
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+};
+
+const usersSchema = mongoose.Schema({
+  userToken: { name: String, token: String }
+});
+
+const User = mongoose.model("User", usersSchema);
 
 app.get("/", async (req, res) => {
   res.json({
@@ -11,19 +29,18 @@ app.get("/", async (req, res) => {
   });
 });
 app.get("/clickuplogin", async (req, res) => {
-  res.redirect(`https://app.clickup.com/api?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`);
-});
-app.get("/slack/clickup/oauth", async (req, res) => {
-  console.log(req.params.code)
-  res.redirect("/clickup/result");
-});
-app.get("/clickup/result", async (req, res) => {
-  res.json({
-    "message": "Success authorized"
+  res.redirect(
+    `https://app.clickup.com/api?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`
+  );
+  app.get("/slack/clickup/oauth", async (req, res) => {
+    res.redirect("/clickup/result");
+  });
+  app.get("/clickup/result", async (req, res) => {
+    res.json({
+      message: "Success authorized",
+    });
   });
 });
-
-
 
 
 
