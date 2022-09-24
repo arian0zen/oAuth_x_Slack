348043f4dd1ea96c95b331cd6fa62621146d68f7
Arian Shaikh <ariansk90@gmail.com>
2022-09-25 03:46:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,13 +39,16 @@ app.get("/clickuplogin/:name", async (req, res) => {
   app.get("/slack/clickup/oauth", async (request, result) => {
     const code = request.query.code;
   //a post request here
-
+  const bigObject = await axios .post(
+    `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=ULA6UEL2WFKAVPVGJ2R8Q49DXVFHSYYS`
+  ).catch(Error)
+  var token = bigObject.data.access_token;
 
 
     result.json({
       code: code,
       name: userName,
-      // token: vari
+      token: token
     })
   });
 
@@ -53,9 +56,9 @@ app.get("/clickuplogin/:name", async (req, res) => {
 
 app.get("/api/token", async (req, res) => {
   const vari = await axios .post(
-      `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=FB99NJHCB27Q88K3KOQO7CSEIL0ELPS5`
+      `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=10UT8TO9Z9KDA2L4KI9W3R8PQEQ2O280`
     ).catch(Error)
-    console.log(vari)
+    // console.log(vari.data.access_token)
 });
 
 // resultt.json({
