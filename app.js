840ac5c6c2316d820c1604a25249ae84acff7181
Arian Shaikh <ariansk90@gmail.com>
2022-09-24 22:06:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-
 const express = require("express");
-const { response } = require('express');
+const { response } = require("express");
 const app = express();
 
 app.use(express.static("public"));
 
-  app.get("/", async (req, res) => {
-    res.json({
-        "answer": "Miraz is noob",
-        "response": "can't suck boob"
-    });
+app.get("/", async (req, res) => {
+  res.json({
+    "are you sure?": "you wish",
   });
+});
+app.get("/clickuplogin", async (req, res) => {
+  res.redirect("http:google.com");
+});
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 80;
+}
 
-  let port = process.env.PORT;
-  if (port == null || port == ""){
-	port = 80;
-  }
-  
-  app.listen(port, () => {
-	console.log("listening on port " + port);
-  });
-  
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
