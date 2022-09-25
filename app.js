cef576c2bc3a3e10041ffe827e4ec52d9d5e12f2
Arian Shaikh <ariansk90@gmail.com>
2022-09-25 19:44:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,62 @@ app.get("/", async (req, res) => {
 });
 
 
+app.get("/clickuplogin/:name", async (req, res) => {
+  var userName_slack = req.params.name;
+  res.redirect(
+    `https://app.clickup.com/api?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`
+  );
+  app.get("/slack/clickup/oauth", async (request, result) => {
+    const code = request.query.code;
+    const bigObject = await axios
+      .post(
+        `https://api.clickup.com/api/v2/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${code}`
+      )
+      .catch(Error);
+    var token = bigObject.data.access_token;
+    // const header_token = {
+    //   headers:{
+    //     'Authorization': token 
+    //   }
+    // }
+    // const bigObject2 = await axios
+    //   .get(
+    //     `https://api.clickup.com/api/v2/user`, header_token
+    //   )
+    //   .catch(Error);
+    // var username = bigObject2.data.user.id
+    let newUSer = new User({
+      name: userName_slack,
+      token: token,
+      clickup_name: 'ar'
+    });
+    newUSer.save().then((item) => {
+      result.json({
+        code: code,
+        message: "success",
+        name: userName_slack
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 80;
